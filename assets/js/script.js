d90ad717fed2c7e8ced8319ee952d950ec5c1422
LilPessy <pettidaniele1f@gmail.com>
2022-09-06 01:50:28 +0200

    Primo commit
    inizializzazione del progetto

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let songs = [
+    {
+        id: 1,
+        name: "COLPA TUA",
+        authors: ["Mace", "Guè"],
+        album: "OBE",
+        path: "lilpessy.github.io/assets/songs/colpa-tua.mp3",
+        cover: "lilpessy.github.io/assets/cover/obe.jpg"
+    },
+];
