e891f725032772de11a628dd1fff43416b663170
LilPessy <pettidaniele1f@gmail.com>
2022-09-06 20:06:22 +0200

    aggiunte cose

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
 let songs = [
     {
         id: 1,
         name: "COLPA TUA",
-        authors: ["Mace", "Guè"],
+        authors: ["Mace", "Guè", "Venerus"],
         album: "OBE",
         path: "https://lilpessy.github.io/player/assets/songs/colpa-tua.mp3",
         cover: "https://lilpessy.github.io/player/assets/cover/obe.jpg"
@@ -17,11 +21,30 @@ let songs = [
     },
 ];
 
-let prev = document.querySelector("prev")
-let play = document.querySelector("play")
-let succ = document.querySelector("succ")
+let prev = document.querySelector(".prev")
+let play = document.querySelector(".play")
+let succ = document.querySelector(".succ")
+let firstPlay = true
 
-play.addEventListener("click", (e)=>{
+let currentSong = null;
+let pause = false;
+
+/*play.addEventListener("click", (e)=>{
     e.preventDefault();
+    if(firstPlay){
+        let i = getRndInteger(0, songs.length);
+        currentSong = new Audio(songs[i].path);
+        currentSong.play();
+        firstPlay = false;
+    } else{
+        if(!pause){
+            currentSong.pause();
+            pause = true
+        }else{
+            currentSong.play();
+            pause = false
+        }
+    }    
     
-})
+});*/
+
